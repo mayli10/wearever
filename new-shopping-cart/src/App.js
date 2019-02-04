88e2497ded926a4180d2eb7ed04d89903d93fc5e
mayli10 <mali@vassar.edu>
2019-02-03 22:41:04 -0600

    added nav bar, styling, and a new component PrimarySearchAppBar.js

--- new-shopping-cart/src/App.js
+++ new-shopping-cart/src/App.js
@@ -1,25 +1,85 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import products from './products.json';
+import PrimarySearchAppBar from './PrimarySearchAppBar.js'
+import Card from '@material-ui/core/Card';
 
 class App extends Component {
   render() {
+    console.log(products)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <PrimarySearchAppBar></PrimarySearchAppBar>
+      </div>
+    );
+  }
+}
+
+class ProductCounter extends Component {
+  render() {
+    let productCount = 1;
+    return (
+      <text>{productCount} Product(s) found.</text>
+    );
+  }
+}
+
+class FilterBar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showMenu: false,
+    };
+
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  showMenu(event) {
+    event.preventDefault();
+
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+
+  closeMenu(event) {
+
+    if (!this.dropdownMenu.contains(event.target)) {
+
+      this.setState({ showMenu: false }, () => {
+        document.removeEventListener('click', this.closeMenu);
+      });
+
+    }
+  }
+  render() {
+    return (
+      <div>
+      Filter
+        <button onClick={this.showMenu}>
+          Show menu
+        </button>
+
+        {
+          this.state.showMenu
+            ? (
+              <div
+                className="menu"
+                ref={(element) => {
+                  this.dropdownMenu = element;
+                }}
+              >
+                <button> Menu item 1 </button>
+                <button> Menu item 2 </button>
+                <button> Menu item 3 </button>
+              </div>
+            )
+            : (
+              null
+            )
+        }
       </div>
     );
   }
