da685c6d95e68aed0d87c7f137fd7711fe5cc046
mayli10 <mayli10x@gmail.com>
2019-04-01 23:50:03 -0500

    added heart icons

--- frontend/src/modules/Products/ProductList/Component.js
+++ frontend/src/modules/Products/ProductList/Component.js
@@ -14,10 +14,16 @@ class ProductList extends Component {
         { isLoading && 'Loading Products' }
         { products && products.map(product =>
           <div key={ product.sku }>
-            <img className={styles.img}
-            src={ product.defaultImages[0] }
-            onMouseOver={e => (e.currentTarget.src = product.defaultImages[1])}
-            onMouseOut={e => (e.currentTarget.src = product.defaultImages[0])}/>
+            <div className={styles.imgContainer}>
+              <img className={styles.img}
+              src={ product.defaultImages[0] }
+              onMouseOver={e => (e.currentTarget.src = product.defaultImages[1])}
+              onMouseOut={e => (e.currentTarget.src = product.defaultImages[0])}/>
+              <img className={styles.heartButton}
+              src="http://localhost:3001/images/icons/empty-heart.png"
+              onMouseOver={e => (e.currentTarget.src = "http://localhost:3001/images/icons/colored-heart.png")}
+              onMouseOut={e => (e.currentTarget.src = "http://localhost:3001/images/icons/empty-heart.png")}/>
+            </div>
             <div className={styles.infoSection}>
               <div className={styles.designer}>{ product.designer.toUpperCase() }</div>
               <div className={styles.title}>{ product.title }</div>
