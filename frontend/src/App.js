2a7670c3c1732bc71aa24aeb077673f34182a839
mayli10 <mayli10x@gmail.com>
2019-04-14 01:50:08 -0500

    renamedfiles and added home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,28 +8,17 @@ import classNames from 'classnames';
 // import { ReactComponent as Logo } from './images/logo.svg';
 import Home from './components/Home';
 import EmailPopup from './components/EmailPopup';
-import NavBar from './modules/NavBar/Container';
-import FilterBar from './modules/FilterBar/Container';
-import SideBar from './modules/SideBar/Container';
-import Counter from './modules/Counter/Container';
+import ProductListPage from './components/ProductListPage';
+import DefaultNavBar from './components/DefaultNavBar';
 import store, { history } from './store';
 
 export default () => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <div>
-        <EmailPopup/>
-        <NavBar/>
-        <div className='main-container'>
-          <SideBar/>
-          <div>
-            <div className='filter-container'>
-              <FilterBar/>
-            </div>
-            <Route exact path="/" component={Home}/>
-          </div>
-        </div>
+        <Route exact path="/" component={Home}/>
         <Route exact path="/signup" component={() => <div>signup page</div>}/>
+        <Route exact path="/productlistpage" component={ProductListPage}/>
       </div>
     </ConnectedRouter>
   </Provider>
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import ProductList from '../modules/Products/ProductList/Container.js';
+import { Route, Link } from 'react-router-dom';
+import classNames from 'classnames';
+import EmailPopup from './EmailPopup';
+import DefaultNavBar from './DefaultNavBar';
+import './Home.css';
+
 
 export const Home = () => (
-  <div>
-    <ProductList/>
+<div>
+  <EmailPopup/>
+  <DefaultNavBar/>
+  <div className='full-container'>
+    <div className='first-row'>
+      <img className='beach-girl' src="http://localhost:3001/images/homepage/beach-girl.jpg"/>
+      <img className='luggage' src="http://localhost:3001/images/homepage/luggage.jpg"/>
+    </div>
+    <div className='second-row'>
+      <div className='second-inner-row'>
+        <img className='selfie' src="http://localhost:3001/images/homepage/selfie.jpg"/>
+      </div>
+      <div className='third-inner-row'>
+        <img className='beach' src="http://localhost:3001/images/homepage/beach.jpg"/>
+        <img className='walk' src="http://localhost:3001/images/homepage/walk.jpg"/>
+      </div>
+      <img className='building' src="http://localhost:3001/images/homepage/building.jpg"/>
+    </div>
   </div>
+</div>
 );
 
 export default Home;
